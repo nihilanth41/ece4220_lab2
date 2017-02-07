6daf420747535eee087c817b105e4750bf2998fe
Zachary Rump <zrrm74@mail.missouri.edu>
2017-02-07 11:06:35 -0600

    Add skeleton code

---
+++ part1/lab2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(int argc, char **argv) {
+	// Get filename arg
+	// Get row/column size from file 
+	// Read matrix a,b from file	
+	// Check matrix dimensons
+
+	// Get start time
+
+	// Create threads (do computation)
+
+	// join threads
+
+	// Get end time
+
+	// print result matrix
+	// print time elapsed
+
+
+	return 0;
+}
