3bef0e80ccae72f0d99b1dd1ad9a5d3ee40224c0
Zachary Rump <zrrm74@mail.missouri.edu>
2017-02-13 15:39:56 -0600

    Fix addition 1 thread per row.
    The issue was that I was only creating 1 arg struct (TOTAL) and passing
    it to all threads. Since the current_row is part of the struct, the
    threads only work correctly if the thread finishes before the
    'current_row' val gets incremented in the main thread.

--- part1/lab2.c
+++ part1/lab2.c
@@ -10,17 +10,17 @@ pthread_mutex_t lock;
 pthread_t threads[SIZE_MAX];
 
 typedef struct matrix_t {
-  int matrix_a[SIZE_MAX][SIZE_MAX];
-  int matrix_b[SIZE_MAX][SIZE_MAX];
-  int matrix_c[SIZE_MAX][SIZE_MAX];
-  int a_rows, a_columns;
-  int b_rows, b_columns;
-  int c_rows, c_columns;
+	int matrix_a[SIZE_MAX][SIZE_MAX];
+	int matrix_b[SIZE_MAX][SIZE_MAX];
+	int matrix_c[SIZE_MAX][SIZE_MAX];
+	int a_rows, a_columns;
+	int b_rows, b_columns;
+	int c_rows, c_columns;
 } matrix_t;
 
 typedef struct matrix_row_t {
-  matrix_t *mtx;
-  int current_row;
+	matrix_t *mtx;
+	int current_row;
 } matrix_rows_t;
 
 // prototypes
@@ -32,263 +32,263 @@ void matrix_add_rows(void *);
 void matrix_add_rows_wrapper(matrix_rows_t *);
 
 
-  
+
 int main(int argc, char **argv) {
- // To store the matricies
-  matrix_t mtx;
-  char opt;
+	// To store the matricies
+	matrix_t mtx;
+	char opt;
 
-  // For measuring elapsed time
-  //struct timeval tv_Before, tv_After;
+	// For measuring elapsed time
+	//struct timeval tv_Before, tv_After;
 
-  if (pthread_mutex_init(&lock, NULL) != 0)
-    {
-      printf("Mutex initialization failed\n");
-      return -1;
-    }
-
-  // Check input parameters
-  validate_input(argc, argv);
-  int ret = open_input_file(argv[1], &mtx);
-  if(ret < 0)
-    {
-      printf("Error opening input file: %s\n", argv[1]);
-      return -1;
-    }
-  
-  opt = *argv[2];
-  if('a' == opt || 'A' == opt)
-    {
-      // Check matrix dimensons
-      if( (mtx.a_columns != mtx.b_columns) || (mtx.a_rows != mtx.b_rows) )
+	if (pthread_mutex_init(&lock, NULL) != 0)
 	{
-	  printf("Invalid dimensions for addition\n");
-	  return -1;
+		printf("Mutex initialization failed\n");
+		return -1;
 	}
-      // Addition
-      // Get start time
-      //gettimeofday(&tv_Before, NULL);
-      // 
-      //int ret =pthread_create(&threads[0], NULL, (void *)&matrix_add_single, (void *)&mtx);
-      //if(ret != 0)
-      //	{
-      //	  printf("Error creating thread\n");
-      //	  return -1;
-      //	}
-      //// Wait for thread to finish
-      //pthread_join(threads[0], NULL);
 
-      // 1 thread per row
-      matrix_rows_t matrix_rows;
-      matrix_rows.mtx = &mtx;
-      matrix_rows.mtx->c_rows = mtx.a_rows;
-      matrix_rows.mtx->c_columns = mtx.a_columns;
-      int i;
-      for(i=1; i<=mtx.a_rows; i++)
+	// Check input parameters
+	validate_input(argc, argv);
+	int ret = open_input_file(argv[1], &mtx);
+	if(ret < 0)
 	{
-		matrix_rows.current_row = i;
-		pthread_create(&threads[i], NULL, (void *)&matrix_add_rows, (void *)&matrix_rows);
-		pthread_join(threads[i], NULL);
+		printf("Error opening input file: %s\n", argv[1]);
+		return -1;
 	}
-      // Get end time
-      // Wait for threads to finish
-      //for(i=1; i<=mtx.a_rows; i++)
-      //	{
-      //	  pthread_join(threads[i], NULL);
-      //	}
-    }
-  else if('m' == opt || 'M' == opt)
-    {
 
-      if(mtx.a_columns != mtx.b_rows)
+	opt = *argv[2];
+	if('a' == opt || 'A' == opt)
 	{
-	  printf("Invalid dimensions for multiplication\n");
-	  return -1;
+		// Check matrix dimensons
+		if( (mtx.a_columns != mtx.b_columns) || (mtx.a_rows != mtx.b_rows) )
+		{
+			printf("Invalid dimensions for addition\n");
+			return -1;
+		}
+		// Addition
+		// Get start time
+		//gettimeofday(&tv_Before, NULL);
+		// 
+		pthread_create(&threads[0], NULL, (void *)&matrix_add_single, (void *)&mtx);
+		// Wait for thread to finish
+		pthread_join(threads[0], NULL);
+		
+		// Print results of single thread addition
+		printf("Results of addition with single thread: \n");
+		int i,j;
+		for(i=1; i<=mtx.c_rows; i++)
+		{
+			// Column
+			for(j=1; j<=mtx.c_columns; j++)
+			{
+				printf("%d ", mtx.matrix_c[i][j]);
+			}
+			printf("\n");
+		}
+
+		// 1 thread per row
+		matrix_rows_t matrix_rows[SIZE_MAX];
+		for(i=1; i<=mtx.a_rows; i++)
+		{
+			matrix_rows[i].mtx = &mtx;
+			matrix_rows[i].mtx->c_rows = mtx.a_rows;
+			matrix_rows[i].mtx->c_columns = mtx.a_columns;
+			matrix_rows[i].current_row = i;
+			pthread_create(&threads[i], NULL, (void *)&matrix_add_rows, (void *)&matrix_rows[i]);
+		}
+		//Get end time
+		//Wait for threads to finish
+		for(i=1; i<=mtx.a_rows; i++)
+		{
+			pthread_join(threads[i], NULL);
+		}
 	}
-      // Multiplication
-      // Get start time
-      pthread_create(&threads[1], NULL, (void *)&matrix_multiply_single, (void *)&mtx);
-      // Wait for thread to finish
-      pthread_join(threads[1], NULL);
-      // Get end time
-    }
-
-  
-  
-	
-
-  // print result matrix
-  int i,j;
-  for(i=1; i<=mtx.c_rows; i++)
-    {
-      // Column
-      for(j=1; j<=mtx.c_columns; j++)
+	else if('m' == opt || 'M' == opt)
 	{
-	  printf("%d ", mtx.matrix_c[i][j]);
+
+		if(mtx.a_columns != mtx.b_rows)
+		{
+			printf("Invalid dimensions for multiplication\n");
+			return -1;
+		}
+		// Multiplication
+		// Get start time
+		pthread_create(&threads[1], NULL, (void *)&matrix_multiply_single, (void *)&mtx);
+		// Wait for thread to finish
+		pthread_join(threads[1], NULL);
+		// Get end time
 	}
-      printf("\n");
-    }
 
-  return 0;
+	// print result matrix
+	printf("Results of addition with 1 thread per row: \n");
+	int i,j;
+	for(i=1; i<=mtx.c_rows; i++)
+	{
+		// Column
+		for(j=1; j<=mtx.c_columns; j++)
+		{
+			printf("%d ", mtx.matrix_c[i][j]);
+		}
+		printf("\n");
+	}
 
-  pthread_mutex_destroy(&lock);
+	return 0;
+
 }
 
 void matrix_add_rows(void *m) {
-  matrix_rows_t *matrix_rows = (matrix_rows_t *)m;
-  matrix_t *mtx = matrix_rows->mtx;
-  int i = matrix_rows->current_row;
-  int j;
-  pthread_mutex_lock(&lock);
-  for(j=1; j<=mtx->a_columns; j++)
-    {
-      mtx->matrix_c[i][j] = mtx->matrix_a[i][j] + mtx->matrix_b[i][j];
-    }
-  pthread_mutex_unlock(&lock);
+	matrix_rows_t *matrix_rows = (matrix_rows_t *)m;
+	matrix_t *mtx = matrix_rows->mtx;
+	int i = matrix_rows->current_row;
+	int j;
+	for(j=1; j<=mtx->a_columns; j++)
+	{
+		mtx->matrix_c[i][j] = mtx->matrix_a[i][j] + mtx->matrix_b[i][j];
+	}
 }
 
 void matrix_add_rows_wrapper(matrix_rows_t *m) {
-  // For each row of the output matrix, spin up a new thread to do the work
-  
-  
+	// For each row of the output matrix, spin up a new thread to do the work
+
+
 }
 
 void matrix_add_single(void *m) {
-  if(NULL == m)
-    {
-      exit(-1);
-    }
-  matrix_t *mtx = (matrix_t *)m;
-  mtx->c_rows = mtx->a_rows;
-  mtx->c_columns = mtx->a_columns;
-  int i,j;
-  // For each row
-  for(i=1; i<=mtx->c_rows; i++)
-    {
-      // For each column
-      for(j=1; j<=mtx->c_columns; j++)
+	if(NULL == m)
 	{
-	  mtx->matrix_c[i][j] = mtx->matrix_a[i][j] + mtx->matrix_b[i][j];
+		exit(-1);
 	}
-    }
-  pthread_exit(0);
+	matrix_t *mtx = (matrix_t *)m;
+	mtx->c_rows = mtx->a_rows;
+	mtx->c_columns = mtx->a_columns;
+	int i,j;
+	// For each row
+	for(i=1; i<=mtx->c_rows; i++)
+	{
+		// For each column
+		for(j=1; j<=mtx->c_columns; j++)
+		{
+			mtx->matrix_c[i][j] = mtx->matrix_a[i][j] + mtx->matrix_b[i][j];
+		}
+	}
+	pthread_exit(0);
 }
 
 void matrix_multiply_single(void *m) {
-  if(NULL == m)
-    {
-      exit(-1);
-    }
-  matrix_t *mtx = (matrix_t *)m;
-  mtx->c_rows = mtx->a_rows;
-  mtx->c_columns = mtx->b_columns;
-  int i,k,l;
-  int sum_tmp=0;
-  // For each row in Matrix A
-  for(i=1; i<=mtx->a_rows; i++)
-    {
-      // For each column in matrix B
-      for(l=1; l<=mtx->b_columns; l++)
+	if(NULL == m)
 	{
-	  // For each row in matrix B
-	  for(k=1; k<=mtx->b_rows; k++)
-	    {
-	      sum_tmp +=  ( (mtx->matrix_a[i][k]) * (mtx->matrix_b[k][l]) );
-	    }
-	  mtx->matrix_c[i][l] = sum_tmp;
-	  sum_tmp = 0;
+		exit(-1);
 	}
-    }
+	matrix_t *mtx = (matrix_t *)m;
+	mtx->c_rows = mtx->a_rows;
+	mtx->c_columns = mtx->b_columns;
+	int i,k,l;
+	int sum_tmp=0;
+	// For each row in Matrix A
+	for(i=1; i<=mtx->a_rows; i++)
+	{
+		// For each column in matrix B
+		for(l=1; l<=mtx->b_columns; l++)
+		{
+			// For each row in matrix B
+			for(k=1; k<=mtx->b_rows; k++)
+			{
+				sum_tmp +=  ( (mtx->matrix_a[i][k]) * (mtx->matrix_b[k][l]) );
+			}
+			mtx->matrix_c[i][l] = sum_tmp;
+			sum_tmp = 0;
+		}
+	}
 
-  pthread_exit(0);
+	pthread_exit(0);
 }
 
 void validate_input(int argc, char **argv) {
-  // Get filename arg
-  if(argc < 3)
-    {
-      printf("Usage is: %s </path/to/file.txt> <A(dd)|M(ultiply)>", argv[0]);
-      exit(-1);
-    }
-  FILE *fp_r = fopen(argv[1], "r");
-  if(NULL == fp_r) 
-    {
-      printf("Error opening file: %s\n", argv[1]);
-      exit(-1);
-    }
-  if(*argv[2] != 'A' && *argv[2] != 'a' && *argv[2] != 'M' && *argv[2] != 'm')
-    {
-      printf("Usage is: %s </path/to/file.txt> <A(dd)|M(ultiply)>", argv[0]);
-      exit(-1);
-    }
+	// Get filename arg
+	if(argc < 3)
+	{
+		printf("Usage is: %s </path/to/file.txt> <A(dd)|M(ultiply)>", argv[0]);
+		exit(-1);
+	}
+	FILE *fp_r = fopen(argv[1], "r");
+	if(NULL == fp_r) 
+	{
+		printf("Error opening file: %s\n", argv[1]);
+		exit(-1);
+	}
+	if(*argv[2] != 'A' && *argv[2] != 'a' && *argv[2] != 'M' && *argv[2] != 'm')
+	{
+		printf("Usage is: %s </path/to/file.txt> <A(dd)|M(ultiply)>", argv[0]);
+		exit(-1);
+	}
 }
 
 int open_input_file(char *filename, matrix_t *m) {
-  FILE *fp_r = fopen(filename, "r");
-  if(NULL == fp_r) 
-    {
-      return -1;
-    }
-  // Get row/column size of matrix A from file 
-  fscanf(fp_r, "%d %d", &(m->a_rows), &(m->a_columns));
-  printf("Matrix A:\nNumber of rows: %d\nNumber of columns: %d\n", m->a_rows, m->a_columns);
-
-  // Read matrix A from file	
-  char buf[SIZE_MAX];
-  int i=1;
-  // Add 2 to rows because the first row doesn't count
-  // and 0-index
-  while( (fgets(buf, SIZE_MAX, fp_r) != NULL) && (i<(m->a_rows)+2) )
-    {
-      // Increment line count
-      i++;
-      // Tokenize the line into numbers
-      char *tok = strtok(buf, " \n");
-      // Keep track of column while tokenizing
-      int j=0;
-      while( tok != NULL )
+	FILE *fp_r = fopen(filename, "r");
+	if(NULL == fp_r) 
 	{
-	  m->matrix_a[i-2][j+1] = atoi(tok);
-	  //printf("(%d, %d): %s\n", i-2, j+1, tok);
-	  //printf("(%d, %d): %d\n", i-2, j+1, m->matrix_a[i-2][j+1]);
-	  tok = strtok(NULL, " \n");
-	  j++;
+		return -1;
 	}
-    }
-	
-  // Matrix B row/column size is in buf at this point
-  char *tok = strtok(buf, " \n");
-  if(tok == NULL)
-    {
-      return -1;
-    }
-  m->b_rows = atoi(tok);
-  m->b_columns = atoi(strtok(NULL, " \n"));
-  printf("Matrix B:\nNumber of rows: %d\nNumber of columns: %d\n", m->b_rows, m->b_columns);
-	
-  // Read matrix B from file	
-  i=1;
-  // Add 2 to rows because the first row doesn't count
-  // and 0-index
-  while( (fgets(buf, SIZE_MAX, fp_r) != NULL) && (i<(m->b_rows)+1) )
-    {
-      // Increment line count
-      i++;
-      // Tokenize the line into numbers
-      char *tok = strtok(buf, " \n");
-      // Keep track of column while tokenizing
-      int j=0;
-      while( tok != NULL )
-	{
-	  m->matrix_b[i-1][j+1] = atoi(tok);
-	  //printf("(%d, %d): %s\n", i-1, j+1, tok);
-	  //printf("(%d, %d): %d\n", i-1, j+1, m->matrix_b[i-1][j+1]);
-	  tok = strtok(NULL, " \n");
-	  j++;
-	}
-    }
+	// Get row/column size of matrix A from file 
+	fscanf(fp_r, "%d %d", &(m->a_rows), &(m->a_columns));
+	printf("Matrix A:\nNumber of rows: %d\nNumber of columns: %d\n", m->a_rows, m->a_columns);
 
-  return 0;
+	// Read matrix A from file	
+	char buf[SIZE_MAX];
+	int i=1;
+	// Add 2 to rows because the first row doesn't count
+	// and 0-index
+	while( (fgets(buf, SIZE_MAX, fp_r) != NULL) && (i<(m->a_rows)+2) )
+	{
+		// Increment line count
+		i++;
+		// Tokenize the line into numbers
+		char *tok = strtok(buf, " \n");
+		// Keep track of column while tokenizing
+		int j=0;
+		while( tok != NULL )
+		{
+			m->matrix_a[i-2][j+1] = atoi(tok);
+			//printf("(%d, %d): %s\n", i-2, j+1, tok);
+			//printf("(%d, %d): %d\n", i-2, j+1, m->matrix_a[i-2][j+1]);
+			tok = strtok(NULL, " \n");
+			j++;
+		}
+	}
+
+	// Matrix B row/column size is in buf at this point
+	char *tok = strtok(buf, " \n");
+	if(tok == NULL)
+	{
+		return -1;
+	}
+	m->b_rows = atoi(tok);
+	m->b_columns = atoi(strtok(NULL, " \n"));
+	printf("Matrix B:\nNumber of rows: %d\nNumber of columns: %d\n", m->b_rows, m->b_columns);
+
+	// Read matrix B from file	
+	i=1;
+	// Add 2 to rows because the first row doesn't count
+	// and 0-index
+	while( (fgets(buf, SIZE_MAX, fp_r) != NULL) && (i<(m->b_rows)+1) )
+	{
+		// Increment line count
+		i++;
+		// Tokenize the line into numbers
+		char *tok = strtok(buf, " \n");
+		// Keep track of column while tokenizing
+		int j=0;
+		while( tok != NULL )
+		{
+			m->matrix_b[i-1][j+1] = atoi(tok);
+			//printf("(%d, %d): %s\n", i-1, j+1, tok);
+			//printf("(%d, %d): %d\n", i-1, j+1, m->matrix_b[i-1][j+1]);
+			tok = strtok(NULL, " \n");
+			j++;
+		}
+	}
+
+	return 0;
 }
 
- 
+
